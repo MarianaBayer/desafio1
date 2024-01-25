f29075aa2c41efc7ef6f059d823e72478fa633b2
MarianaBayer <157656317+MarianaBayer@users.noreply.github.com>
2024-01-25 17:55:41 -0300

    Add files via upload

---
+++ Desafio 1/logica-js-aula_2/app.js
@@ -0,0 +1,11 @@
+alert('Boas vindas ao nosso site');
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
+let idade = prompt('Digite a sua idade');
+let nome = prompt('Digite seu nome');
+
+if (idade >= 18) {
+    alert('Pode tirar sua habilitação!');
+    }
+
+
